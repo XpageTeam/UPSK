757a432b6490d057955a8bc7f6dd7fce5aeea54c
danil.khandys <danil.khandys@xpage.ru>
2019-07-24 16:06:35 +0500

    1232

--- src/ts/main-partners.ts
+++ src/ts/main-partners.ts
@@ -1,10 +1,10 @@
 import {domReady, App} from "./xpage/index";
 import {Swiper, Lazy, Navigation} from 'swiper/dist/js/swiper.esm.js'
+import {TweenLite} from "gsap"
 
 Swiper.use([Lazy, Navigation])
 
 domReady(() => {
-
 	App.each(".main-partners__slider", (el: HTMLElement) => {
 		new Swiper(el.querySelector(".partners-slider") as HTMLElement, {
 			slidesPerView: 6,
@@ -18,4 +18,62 @@ domReady(() => {
 			}
 		})
 	})
+
+	const $body = window.get$("body");
+
+	$body.on("mousemove", ".partners-slider .swiper-slide", function(e: MouseEvent){
+		partnersSlideAnimate(window.get$(this), e)
+	})
+
+	$body.on("mouseleave", ".partners-slider .swiper-slide", function(e: MouseEvent){
+		partnersSlideAnimate(window.get$(this), e)
+	})
 })
+
+const partnersSlideAnimate = ($slide: any, e: MouseEvent): void => {
+	const targetSlides = {
+		prev: $slide.prev(".swiper-slide").find(".partner-slide")[0],
+		current: $slide.find(".partner-slide")[0],
+		next: $slide.next(".swiper-slide").find(".partner-slide")[0]
+	};
+
+	setScaleByCursorPos(targetSlides, e)
+},
+
+setScaleByCursorPos = (targetSlides: any, e: MouseEvent): void => {
+	const partOfElement = parseInt((targetSlides.current.clientWidth / 2).toString());
+
+	const scaleCurrent = parseFloat((e.offsetX <= partOfElement
+				? e.offsetX / partOfElement
+				: (targetSlides.current.clientWidth - e.offsetX) / partOfElement)
+				.toFixed(2)) / 4;
+
+	// console.log(e)
+
+	// const scalePrev = parseFloat((e.offsetX <= partOfElement 
+	// 				? partOfElement / e.offsetX 
+	// 				: partOfElement / (targetSlides.current.clientWidth - e.offsetX))
+	// 				.toFixed(2)) / 4;
+
+	TweenLite.to(targetSlides.current.querySelector("img"), .1, {
+		scale: 1 + Math.abs(scaleCurrent)
+	})
+
+	// if (e.offsetX <= partOfElement){
+	// 	TweenLite.to(targetSlides.prev, .1, {
+	// 		scale: 1 + scalePrev / 3
+	// 	})
+	// 	TweenLite.to(targetSlides.next, .1, {
+	// 		scale: 1
+	// 	})
+	// }else{
+	// 	TweenLite.to(targetSlides.next, .1, {
+	// 		scale: 1 + scalePrev/ 3
+	// 	})
+	// 	TweenLite.to(targetSlides.prev, .1, {
+	// 		scale: 1
+	// 	})
+	// }
+
+
+}
